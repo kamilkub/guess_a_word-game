1513ecfc2e399442d422a756e5a3ed5c95c89a7c
kamilkub <37770696+kamilkub@users.noreply.github.com>
2019-02-17 10:04:32 +0100

    Add files via upload

---
+++ src/guess/word/pckg/WordLength.java
@@ -0,0 +1,44 @@
+package guess.word.pckg;
+
+import java.util.ArrayList;
+
+public class WordLength {
+   
+	
+	private int length;
+	
+	private int timeOfGuesses;
+	
+	
+	public WordLength () {
+		
+		
+	}
+	
+	
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+
+	public int getTimeOfGuesses() {
+		return timeOfGuesses;
+	}
+
+
+	public void setTimeOfGuesses(int timeOfGuesses) {
+		this.timeOfGuesses = timeOfGuesses;
+	}
+
+	
+	
+	
+
+	
+	
+	
+}
